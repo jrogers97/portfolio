3fa46761e900a6e060993756f185fb24971100e7
Jake Rogers <jake@dataplusmath.com>
2018-02-27 21:51:51 -0500

    added favicons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 $(window).scroll(function() {
+
+
+  // make nav sticky
   var mn = $('.nav');
 
   if($(this).scrollTop() > $(window).height()) { 
@@ -7,7 +10,7 @@ $(window).scroll(function() {
      mn.removeClass("nav-scrolled")
   };
 
-
+  // highlight current nav item
   var scroll_top = $(window).scrollTop();
   var wh = $(window).height();
   var about_ot = $('.about').offset().top - 50;
@@ -16,48 +19,29 @@ $(window).scroll(function() {
   var contact_ot = $('.contact').offset().top - 50;
 
   if (scroll_top < wh) {
-  	console.log('home');
+  	$('.nav-link, .nav-link-home').removeClass('current-nav');
   	$('#home').addClass('current-nav');
-  	$('#about').removeClass('current-nav');
-  	$('#projects').removeClass('current-nav');
-  	$('#resume').removeClass('current-nav');
-  	$('#contact').removeClass('current-nav');
   } else if (scroll_top > wh && scroll_top < projects_ot) {
-  	console.log('about');
-  	$('#home').removeClass('current-nav');
+  	$('.nav-link, .nav-link-home').removeClass('current-nav');
   	$('#about').addClass('current-nav');
-  	$('#projects').removeClass('current-nav');
-  	$('#resume').removeClass('current-nav');
-  	$('#contact').removeClass('current-nav');
   } else if (scroll_top > projects_ot && scroll_top < resume_ot) {
-  	console.log('projects')
-  	$('#home').removeClass('current-nav');
-  	$('#about').removeClass('current-nav');
+  	$('.nav-link, .nav-link-home').removeClass('current-nav');
   	$('#projects').addClass('current-nav');
-  	$('#resume').removeClass('current-nav');
-  	$('#contact').removeClass('current-nav');
   } else if (scroll_top > resume_ot && scroll_top < contact_ot) {
-  	console.log('resume');
-  	$('#home').removeClass('current-nav');
-  	$('#about').removeClass('current-nav');
-  	$('#projects').removeClass('current-nav');
+  	$('.nav-link, .nav-link-home').removeClass('current-nav');
   	$('#resume').addClass('current-nav');
-  	$('#contact').removeClass('current-nav');
   } else if (scroll_top > contact_ot) {
-  	console.log('contact');
-  	$('#home').removeClass('current-nav');
-  	$('#about').removeClass('current-nav');
-  	$('#projects').removeClass('current-nav');
-  	$('#resume').removeClass('current-nav');
+  	$('.nav-link, .nav-link-home').removeClass('current-nav');
   	$('#contact').addClass('current-nav');
   }
 });
 
-// Nav link scroll functions
-$(document).ready(function() {
-	$(".nav-link").click(function(e) {
 
+$(document).ready(function() {
+	// Nav link scroll to section
+	$(".nav-link").click(function(e) {
 	  e.preventDefault();
+
 	  var item_id = this.id;
 	   
 	    $('html, body').animate({
@@ -65,12 +49,45 @@ $(document).ready(function() {
 	    }, 800);
 	});
 
+	// Nav link scroll to top
 	$(".nav-link-home").click(function(e) {
-
 	  e.preventDefault();
 	   
 	    $('html, body').animate({
 	        scrollTop: 0
 	    }, 800);
 	});
+
+	// Learn more button scroll to about
+	$(".learn-more").click(function(e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+	        scrollTop: $(window).height() + 1
+	    }, 800);
+	});
+
+	// Reveal music section dropdown
+	$('.yes-button').click(function(e) {
+		e.preventDefault();
+
+		
+		$('.yes-button').fadeTo(200,0.0);
+		setTimeout(function() {
+			$('.yes-button').addClass('hidden');
+			$('.music-expand').removeClass('hidden');
+			$('.music-expand').animate({
+				height: 194
+			},400);
+		},300);
+
+		setTimeout(function() {
+			$('.music-expand-content').removeClass('hidden');
+			$('.music-expand-content').animate({
+				opacity: 1
+			},400);
+		}, 700);
+
+	})
 });
+
